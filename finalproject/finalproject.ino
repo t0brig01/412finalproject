9c162a6c9d2d2af8dfb513ba3ff1dfea040c2ebf
MikeAlfaWisky <49526765+MikeAlfaWisky@users.noreply.github.com>
2019-04-23 16:42:15 -0400

    Update finalproject.ino

--- finalproject/finalproject.ino
+++ finalproject/finalproject.ino
@@ -1,67 +1,59 @@
-  #include<LiquidCrystal.h>
 
-LiquidCrystal lcd(12,13,8,9,10,11); //initalize LCD
+#include<LiquidCrystal.h>
+LiquidCrystal lcd(12,13,8,9,10,11); //sets pins for lcd
+#define SENSORPIN 2 //dime
+#define SENSORPIN2 3 //nickel
+#define SENSORPIN3 4 //quarter
 
-const int interruptPinQuarter = 2; 
-const int interruptPinDime = 3;
-const int interruptPinNickel = 4;  
-const int interruptPinPenny = 5;  
-
-int sum = 0;
-bool changed = true;
-
-void setup() {
-  // put your setup code here, to run once:
-  lcd.begin(16,2);
-  lcd.display();
-
-  pinMode(interruptPinQuarter,INPUT);
-  pinMode(interruptPinDime,INPUT);
-  pinMode(interruptPinNickel,INPUT);
-  pinMode(interruptPinPenny,INPUT);
-
-  attachInterrupt(digitalPinToInterrupt(interruptPinQuarter), brokenQuarter, FALLING);
-  attachInterrupt(digitalPinToInterrupt(interruptPinDime), brokenDime, FALLING);
-  attachInterrupt(digitalPinToInterrupt(interruptPinNickel), brokenNickel, FALLING);
-  attachInterrupt(digitalPinToInterrupt(interruptPinPenny), brokenPenny, FALLING);
-
+int sensorState = 0;
+int lastState = 0;
+int sensorState2 = 0;
+int sensorState3 = 0;
+int sum; // keeps track of current money total
+void setup() {      
+  pinMode(SENSORPIN, INPUT); //initialize sensor as input 
+  pinMode(SENSORPIN2, INPUT);//initialize sensor as input
+  pinMode(SENSORPIN3, INPUT);//initialize sensor as input     
+  digitalWrite(SENSORPIN, HIGH); 
+  digitalWrite(SENSORPIN2, HIGH);
+  digitalWrite(SENSORPIN3, HIGH);
+  
   Serial.begin(9600);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  if (changed == true){
+ 
+void loop(){
+  sensorState = digitalRead(SENSORPIN); //reads sensor value
+  sensorState2 = digitalRead(SENSORPIN2); //reads sensor value
+  sensorState3 = digitalRead(SENSORPIN3); //reads sensor value
+ 
+  if (sensorState == LOW) { //checks if sensor 1 beam is broken    
+    delay(500);
+    sum += 10;
     displayLCD();
-    Serial.println(sum);
-    changed = false;
+ 
+  } 
+
+   if (sensorState2 == LOW){ //checks if sensor 2 beam is broken
+    delay(500);
+    sum += 5;
+    displayLCD();
   }
+  if(sensorState3 == LOW) { //checks if sensor 3 beam is broken
+    delay(500);
+    sum += 25;
+    displayLCD();
+  }
+ 
+  if (sensorState && !lastState) {
+    Serial.println("Unbroken");
+  } 
+  if (!sensorState && lastState) {
+    Serial.println("Broken");
+  }
+  lastState = sensorState;
 }
-void displayLCD(){
+void displayLCD(){ //function to display total value
   lcd.clear();
   lcd.print("Total:");
   lcd.print(sum);
-}
-void brokenQuarter(){
-  changed = true;
-  delay(500);
-  sum += 25;
-  Serial.println("Break - Quarter");
-}
-void brokenDime(){
-  changed = true;
-  delay(500);
-  sum += 10;
-  Serial.println("Break - Dime");
-}
-void brokenNickel(){
-  changed = true;
-  delay(500);
-  sum += 5;
-  Serial.println("Break - Nickel");
-}
-void brokenPenny(){
-  changed = true;
-  delay(500);
-  sum += 1;
-  Serial.println("Break - Penny");
 }
